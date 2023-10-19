137480e72663aa8e3fb7face3f07d5fe5f1bf20e
liuxuan <liuxuan@163.com>
2023-10-19 10:08:18 +0800

    1.2.2

--- SSPAdFramework.framework/Headers/SASplashAd.h
+++ SSPAdFramework.framework/Headers/SASplashAd.h
@@ -136,6 +136,7 @@
 
 @property (nonatomic, weak) UIViewController * _Nullable rootViewController;
 
+@property (nonatomic, weak) UIWindow * _Nullable window;
 
 @end
 
--- SSPAdSDK.podspec
+++ SSPAdSDK.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "SSPAdSDK"
-  s.version      = "1.2.1"
+  s.version      = "1.2.2"
   s.summary      = "SSPAdSDK"
 
 
